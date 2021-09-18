3186847ee8df9108e3a941d0e2ef500f1275d4e4
lgfh98 <lgfigueroah@outlook.com>
2021-09-18 15:13:12 -0500

    commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0eeb473cbd03125c09081f0ceb611bf3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6d59ff75-773f-489c-86de-78fc298c29c4'
+};
